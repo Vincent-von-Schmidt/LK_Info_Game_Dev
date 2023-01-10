1776bc44cd6d5924af4c6ad5da852efae2e62fb8
unknown <arni.pfueller@gmail.com>
2023-01-10 13:43:36 +0100

    fixedBullets

--- src/sprites/bullets.py
+++ src/sprites/bullets.py
@@ -8,9 +8,17 @@ class Bullets:
     def __init__(self, pos_x, pos_y, dir) -> None:
         
         super().__init__()
+
+        #horizontal bullets
         self.image = pygame.Surface((50, 10))
         self.image.fill((255, 0, 0))
         self.rect = self.image.get_rect(center = (pos_y, pos_x))
+
+        #vertical bullets
+        self.image2 = pygame.Surface((10, 50))
+        self.image2.fill((0, 255, 0))
+        self.rect2 = self.image.get_rect(center =(pos_x, pos_y))
+
         self.end = False
         self.vel = 1000
         self.dir = dir
@@ -26,17 +34,24 @@ class Bullets:
             self.rect.x -= 5 
         
         elif self.dir == core.UP:
-            self.rect.y -= 5 
+            self.rect2.y -= 5 
 
         elif self.dir == core.DOWN:
-            self.rect.y += 5 
+            self.rect2.y += 5 
 
     def kill(self): 
         if self.rect.x >= 200:
             self.end = True
+        
+        elif self.rect.y >= 500: 
+            self.end = True 
     
     def render(self): 
-        return [(self.image, (self.rect.centerx, self.rect.centery))]
+        if self.dir == core.RIGHT or self.dir == core.LEFT:
+            return [(self.image, (self.rect.centerx, self.rect.centery))] 
+        else: 
+            return [(self.image2, (self.rect2.centerx, self.rect2.centery))]
+        
 
 
 
