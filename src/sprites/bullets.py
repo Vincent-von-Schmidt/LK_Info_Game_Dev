dba40ae76191ba586f1005cbc95157ed932b9f9e
unknown <valentin.schaar@web.de>
2023-01-10 15:10:15 +0100

    Changed format.

--- src/sprites/bullets.py
+++ src/sprites/bullets.py
@@ -9,14 +9,16 @@ class Bullets:
         
         super().__init__()
 
-        #horizontal bullets
+        # Horizontal bullets
+        
         self.image_right = pygame.image.load("./assets/bullets/arrow_right.png")
         self.rect_right = self.image_right.get_rect(center = (pos_x, pos_y))
 
         self.image_left = pygame.image.load("./assets/bullets/arrow_left.png")
         self.rect_left = self.image_left.get_rect(center = (pos_x, pos_y))
 
-        #vertical bullets
+        # Vertical bullets
+
         self.image_up = pygame.image.load("./assets/bullets/arrow_up.png")
         self.rect_up = self.image_up.get_rect(center =(pos_x, pos_y))
 
@@ -44,6 +46,7 @@ class Bullets:
             self.rect_down.y += 5 
 
     def kill(self): 
+        
         if self.rect_right.x >= 200:
             self.end = True
         
@@ -56,13 +59,17 @@ class Bullets:
         elif self.rect_down.y >= 500: 
             self.end = True 
     
-    def render(self): 
+    def render(self):
+
         if self.dir == core.RIGHT:
-            return [(self.image_right, (self.rect_right.centerx, self.rect_right.centery))] 
+            return [(self.image_right, (self.rect_right.centerx, self.rect_right.centery))]
+        
         elif self.dir == core.LEFT: 
             return [(self.image_left, (self.rect_left.centerx, self.rect_left.centery))]
+        
         elif self.dir == core.UP: 
             return [(self.image_up, (self.rect_up.centerx, self.rect_up.centery))]
+        
         elif self.dir == core.DOWN: 
             return [(self.image_down, (self.rect_down.centerx, self.rect_down.centery))]
         
