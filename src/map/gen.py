90769e78d7712493bc579e60cc40b1e1f25d1578
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2023-01-10 11:09:35 +0100

    textures dict

--- src/map/gen.py
+++ src/map/gen.py
@@ -7,15 +7,23 @@ class Room:
         entrance: list = [False, False, False, False],
     ) -> None:
 
-        self.surface: pygame.surface.Surface = pygame.suface.Surface( (240, 176) ) # 14 * 10
+        self.surface: pygame.surface.Surface = pygame.surface.Surface( (240, 176) ) # 14 * 10
         self.surface.fill( (0, 0, 0) )
 
         # textures
-        self.wall_north = pygame.image.load("./assets/wall_north.png")
-        self.wall_east = pygame.image.load("./assets/wall_east.png")
-        self.wall_south = pygame.imgae.load("./assets/wall_south.png")
-        self.wall_west = pygame.image.load("./assets/wall_west.png")
-        self.ground = pygame.image.load("./assets/ground.png")
+        # self.textures: dict = {
+        #     "wall_north" : pygame.image.load("./assets/wall_north.png"),
+        #     "wall_east" : pygame.image.load("./assets/wall_east.png"),
+        #     "wall_south" : pygame.imgae.load("./assets/wall_south.png"),
+        #     "wall_west" : pygame.image.load("./assets/wall_west.png"),
+        #     "ground" : pygame.image.load("./assets/ground.png"),
+        # }
+
+        self.wall_north = 0
+        self.wall_east = 1
+        self.wall_south = 2
+        self.wall_west = 3
+        self.ground = 4
 
         self.tilemap: list = []
         
@@ -28,5 +36,11 @@ class Room:
 
         for _ in range( 14 ): self.tilemap.append( self.wall_south )
 
+        print( f"{self.tilemap = }" )
+
     def render( self ) -> None:
         ...
+
+if __name__ == "__main__":
+    test = Room()
+
