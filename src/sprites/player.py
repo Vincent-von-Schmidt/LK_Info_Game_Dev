e53ca2d10706d5bf18d29b927bd49799c2a7e94c
unknown <valentin.schaar@web.de>
2023-01-10 10:25:14 +0100

    Player is a sprite.

--- src/sprites/player.py
+++ src/sprites/player.py
@@ -1,9 +1,10 @@
 import pygame
 
 import core
+import sprites.sprite
 
 
-class Player:
+class Player(sprites.sprite.Sprite):
     """Player class."""
 
     def __init__(self) -> None:
@@ -21,91 +22,10 @@ class Player:
         self.y = 240
         self.pos = core.RIGHT
 
-        self.image_up = pygame.image.load("./assets/player_up.png")
-        self.image_down = pygame.image.load("./assets/player_down.png")
-        self.image_left = pygame.image.load("./assets/player_left.png")
-        self.image_right = pygame.image.load("./assets/player_right.png")
-
-    def move_up(
-        self, elapsed_time: float = 0, pixel: float = 0, still=False
-    ) -> float:
-        """Move the player up."""
-
-        ################################################################
-        # 1. Change the player position.                               #
-        ################################################################
-
-        dis = (self.v * elapsed_time) + pixel
-        self.y -= dis
-
-        if not still:
-            self.pos = core.UP
-
-        return dis
-    
-    def move_down(
-        self, elapsed_time: float = 0, pixel: float = 0, still=False
-    ) -> float:
-        """Move the player up."""
-
-        ################################################################
-        # 1. Change the player position.                               #
-        ################################################################
-
-        dis = (self.v * elapsed_time) + pixel
-        self.y += dis
-
-        if not still:
-            self.pos = core.DOWN
-
-        return dis
-    
-    def move_left(
-        self, elapsed_time: float = 0, pixel: float = 0, still=False
-    ) -> float:
-        """Move the player up."""
-
-        ################################################################
-        # 1. Change the player position.                               #
-        ################################################################
-
-        dis = (self.v * elapsed_time) + pixel
-        self.x -= dis
-
-        if not still:
-            self.pos = core.LEFT
-
-        return dis
-    
-    def move_right(
-        self, elapsed_time: float = 0, pixel: float = 0, still=False
-    ) -> float:
-        """Move the player up."""
-
-        ################################################################
-        # 1. Change the player position.                               #
-        ################################################################
-
-        dis = (self.v * elapsed_time) + pixel
-        self.x += dis
-
-        if not still:
-            self.pos = core.RIGHT
-
-        return dis
-    
-    def render(self) -> list[tuple[pygame.surface.Surface, tuple[float]]]:
-        """Render the player graphic."""
-
-        if self.pos == core.RIGHT:
-            return [(self.image_right, (self.x, self.y))]
-        
-        elif self.pos == core.LEFT:
-            return [(self.image_left, (self.x, self.y))]
-        
-        elif self.pos == core.UP:
-            return [(self.image_up, (self.x, self.y))]
-        
-        elif self.pos == core.DOWN:
-            return [(self.image_down, (self.x, self.y))]
-
+        super().__init__(
+            speed = self.v,
+            image_up = pygame.image.load("./assets/player_up.png"),
+            image_down = pygame.image.load("./assets/player_down.png"),
+            image_left = pygame.image.load("./assets/player_left.png"),
+            image_right = pygame.image.load("./assets/player_right.png")
+        )
