a96f6008ece2f09c0ae9ded9b692d5894cbe35f4
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2023-01-10 22:42:16 +0100

    hightmap tests

--- src/map/tiles.py
+++ src/map/tiles.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import json
 import pygame
 
 
@@ -17,8 +18,8 @@ class Tile:
 
 
 class TilesMap:
-    def __init__( self, heightmap: str ) -> None:
-        self.surface: pygame.surface.Surface = pygame.surface.Surface( (16, 16) )
+    def __init__( self, heightmap: str = "./test.json" ) -> None:
+        self.surface: pygame.surface.Surface = pygame.surface.Surface( (240, 160) )
 
         self.tiles: dict = {
             "wall_north": Tile( texture = "./assets/map/wall_north.png", collision = True ),
@@ -30,9 +31,22 @@ class TilesMap:
             "block": Tile( texture = "./assets/map/block.png", collision = True ),
         }
 
-        self.tile_construct: list = self.load_height_map(heightmap)
+        self.load_height_map(heightmap)
 
-    def load_height_map( self, heightmap: str ) -> list:
+    def __map_tiles_to_binary( self, content: list ) -> list:
+        output: list = []
+
+        for i in content:
+            for bin in i:
+                
+                match bin:
+                    case 0: output.append( self.tiles["ground"] )
+                    case 1: output.append( self.tiles["block"] )
+                    case _: raise FileExistsError( "File not in binary." )
+
+        return output
+
+    def load_height_map( self, heightmap: str ) -> None:
         
         tile_construct: list = []
 
@@ -44,7 +58,13 @@ class TilesMap:
 
         # between
         with open( heightmap, "r" ) as file:
-            print(file.read())
+            content: list = map( self.__map_tiles_to_binary, json.loads(file.read()) )
+
+        for i in range( 8 ):
+            tile_construct.append( tmp := [] )
+            tmp.append( self.tiles["wall_west"] )
+            for a in range( 13 ): tmp.append( content[i][a] )
+            tmp.append( self.tiles["wall_east"] )
         
         # last row
         tile_construct.append( tmp := [] )
@@ -52,29 +72,25 @@ class TilesMap:
         for _ in range( 13 ): tmp.append( self.tiles["wall_south"] )
         tmp.append( self.tiles["edge"] )
 
-        return tile_construct
+        self.__render(tile_construct)
 
-    def __render( self ) -> None:
+    def __render( self, constuct: list ) -> None:
         """Render the map. -> Draws the tiles on the map surface."""
 
-        for row_index, row in enumerate( self.tile_construct ):
+        for row_index, row in enumerate( constuct ):
             for column_index, tile in enumerate( row ):
                 self.surface.blit(
-                    source = tile,
+                    source = tile.surface,
                     dest = (
-                        column_index * tile.get_width(),
-                        row_index * tile.get_height() 
+                        column_index * tile.surface.get_width(),
+                        row_index * tile.surface.get_height() 
                     ) 
                 )
 
-        # save rendered map, set draw offset -> x = 0, y = 32
-        self.rendered_map = [(self.surface, (0, 32))] 
-
-    def get_map( self ) -> pygame.surface.Surface:
-        return self.surface
+    def get_map( self ) -> list:
+        # offset: x = 0, y = 32
+        return [(self.surface, (0, 32))]
 
 
 if __name__ == "__main__":
-    ...
-    # map: TilesMap = TilesMap()
-    # map.load_height_map("./assets/map/heightmaps/blank.pbm")
+    map: TilesMap = TilesMap()
