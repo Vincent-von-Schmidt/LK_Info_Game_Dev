ded5bcabb1b58759f4e669ca9816e6c19cb1d8de
unknown <valentin.schaar@web.de>
2023-01-11 16:51:07 +0100

    Id for every entity.

--- src/sprites/entity.py
+++ src/sprites/entity.py
@@ -5,5 +5,9 @@ import core
 
 class Entity:
 
+    max_id = 0
+
     def __init__(self):
-        ...
+        
+        Entity.max_id += 1
+        self.id = Entity.max_id
--- src/sprites/player.py
+++ src/sprites/player.py
@@ -1,6 +1,7 @@
 import pygame
 
 import core
+import entity
 import sprites.sprite as sprite
 
 class Player(sprite.Sprite):
