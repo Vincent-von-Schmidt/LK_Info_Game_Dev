6938258c6c219a063c929d43bdcb9214edcf41fb
unknown <valentin.schaar@web.de>
2023-01-11 19:50:20 +0100

    A few merges.

---
+++ src/entity.py
@@ -0,0 +1,34 @@
+import pygame
+
+import core
+
+
+class Entity:
+
+    max_id = 0
+
+    def __init__(self, **kwargs) -> None:
+        
+        Entity.max_id += 1
+        self.id = Entity.max_id
+
+        self.x = kwargs["x"]
+        self.y = kwargs["y"]
+
+        self.rect = pygame.Rect(0, 0, 0, 0)
+        self.fac = kwargs["fac"]
+    
+    def init_rect(self, w, h) -> None:
+        
+        self.rect[0] = self.x
+        self.rect[1] = self.y
+        self.rect[2] = w
+        self.rect[3] = h
+    
+    def update_rect(self) -> None:
+
+        self.rect[0] = self.x
+        self.rect[1] = self.y
+    
+    def render(self) -> list[tuple[pygame.surface.Surface, tuple[float]]]:
+        return [(self._render(), (self.x, self.y))]
---
+++ src/objects/player.py
@@ -0,0 +1,54 @@
+import pygame
+
+import core
+import entity
+import sprites.player
+
+class Player(entity.Entity, sprites.player.Player):
+    """Player class."""
+
+    def __init__(self, **kwargs) -> None:
+        """Initialisation of player objects and variables."""
+
+        ################################################################
+        # 1. Set the start properties.                                 #
+        ################################################################
+
+        # Sprite
+
+        entity.Entity.__init__(self, **kwargs)
+        sprites.player.Player.__init__(
+            self,
+            dir = core.DOWN,
+            speed = 30,
+            change = 0.2,
+            **kwargs,
+        )
+
+        # Properties
+
+        self.attack_block = 1
+        self.shooting = True
+        self.attack_last = 0 # Time
+        self.max_health = 3
+        self.health = 1.5
+
+    def update(self, elapsed_time: float):
+
+        self.attack_last += elapsed_time
+
+        if self.attack_last >= self.attack_block:
+            self.shooting = True
+        
+        else:
+            self.shooting = False
+    
+    def update_health(self, health: float) -> None:
+        """Update the player's health."""
+
+        return None
+    
+    def check_health(self) -> None:
+        """Check the player's health."""
+
+        return None
