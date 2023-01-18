705ad2b16977ee254971a4226e5db9fad5db6e63
unknown <valentin.schaar@web.de>
2023-01-18 23:03:55 +0100

    Removed old funcs.

--- src/core.py
+++ src/core.py
@@ -33,8 +33,3 @@ FAUNA = "fauna"
 SPECIAL = "special"
 
 QUIT = "quit"
-
-# Funktions
-
-def _distance_two_points(x1: float, y1: float, x2: float, y2: float) -> float:
-    return ( (x2-x1)**2 + (y2-y1)**2 )**0.5
